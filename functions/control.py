65f66d8fcf823a14d39a114b440d01ce34498622
SimoneSucci <s253082@dtu.dk>
2026-04-09 09:28:44 +0200

    fixed angle convertion in control function

--- functions/control.py
+++ functions/control.py
@@ -1,11 +1,13 @@
-def update_pitch(theta_pitch, thetaI_old, omeg, omega_ref, KK, KP, KI, dt, theta_min, theta_max):
-    GK = 1/(1+theta_pitch/KK)
-    thetaP = GK*KP*(omeg-omega_ref)
-    thetaI = thetaI_old + GK*KI*(omeg-omega_ref)*dt
-    thetaSP = thetaP + thetaI
+import numpy as np
 
-    thetaI = max(thetaI, theta_min)
-    thetaI = min(thetaI, theta_max)
+def update_pitch(theta_pitch, thetaI_old, omeg, omega_ref, KK, KP, KI, dt, theta_min, theta_max):
+    GK = 1/(1+theta_pitch/KK) #tetha pitch and KK in deg 
+    thetaP = GK*KP*(omeg-omega_ref) #rad
+    thetaI = thetaI_old + GK*KI*(omeg-omega_ref)*dt #rad
+    thetaSP = np.rad2deg(thetaP + thetaI) #return setpoit in deg
+
+    thetaI = max(thetaI, np.deg2rad(theta_min))
+    thetaI = min(thetaI,  np.deg2rad(theta_max))
     thetaSP = max(thetaSP, theta_min)
     thetaSP = min(thetaSP, theta_max)
 
@@ -15,7 +17,7 @@ def calculate_MG(omega, P_rated, K, omega_rated):
     if omega<omega_rated:
         MG = K*omega**2
     else:
-        MG = P_rated/omega_rated
+        MG = P_rated/(omega)
 
     return MG
 
