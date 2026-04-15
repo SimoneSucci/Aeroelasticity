718efc441b86ab989f2218615625619083bf5664
Ombeline <s252973@student.dtu.dk>
2026-04-15 16:48:51 +0200

    Added thhe rungeKunta, it works now for the cart example

--- Newton.py
+++ Newton.py
@@ -19,7 +19,15 @@ t = np.linspace (0,10,200)
 M = 1
 m = 0.5
 L = 2
-g = 9.81
+g_grav = 9.81
+
+#integration intialisation
+dt = 0.1
+N = 100
+y_arr = np.zeros((N,2))
+y_d_arr = np.zeros((N,2))
+y_dd_arr = np.zeros((N,2))
+time = np.zeros(N)
 
 def calculate_dydt(y,t,M,m,L):
 
@@ -34,7 +42,7 @@ def calculate_dydt(y,t,M,m,L):
                           ])
     
     F = np.array([[0.5*m*L**2*theta_d**2*np.cos(theta)],
-                  [0.5*m*L**2*g*np.cos(theta)]
+                  [0.5*m*L**2*g_grav*np.cos(theta)]
                   ])
     
     acc = np.linalg.inv (M_matrix) @ F
@@ -45,27 +53,8 @@ def calculate_dydt(y,t,M,m,L):
 
     return dy_dt
 
-sol = odeint (calculate_dydt,Y0,t, args=(M,m,L))
 
-plt.figure()
-plt.plot(t,sol[:,0],label = 'x')
-plt.plot(t,sol[:,2],label = 'theta')
-plt.legend()
-
-plt.figure()
-plt.plot(t,sol[:,1],label = 'x_d')
-plt.plot(t,sol[:,3],label = 'theta_d')
-plt.legend()
-plt.show()
-
-
-t = 0
-dt = 0.1
-y = np.zeros(2)
-y_d = np.zeros(2)
-
-
-def calculate_g(y,t_d,M,m,L):
+def calculate_g(y,y_d,M,m,L):
 
     x = y[0]
     x_d = y_d[0]  
@@ -79,23 +68,64 @@ def calculate_g(y,t_d,M,m,L):
                           ])
     
     F = np.array([[0.5*m*L**2*theta_d**2*np.cos(theta)],
-                  [0.5*m*L**2*g*np.cos(theta)]
+                  [0.5*m*L**2*g_grav*np.cos(theta)]
                   ])
     
     g = np.linalg.inv (M_matrix) @ F
+    g = g.flatten()
 
     return g
 
-def rungeKutta(dt,y,y_d,M,m,L):
-
-    y_dd = calculate_g(y,y_d,M,m,L)
+def rungeKutta(dt,t,y,y_d,y_dd,M,m,L):
 
     A = dt*y_dd/2
     b = dt *(y_d+0.5*A)/2
-    
+
+    g2 = calculate_g(y+b,y_d+A,M,m,L)
+    B = dt*g2/2
+    g3 = calculate_g(y+b,y_d+B,M,m,L)
+    C = dt*g3/2
+
+    d = dt*(y_d+C)
+    g4 = calculate_g(y+d, y_d+2*C,M,m,L)
+    D = dt*g4/2
+
+    y_new = y + dt*(y_d+(A+B+C)/3)
+    y_d_new = y_d + ((A+2*B+2*C+D)/3)
+    y_dd_new = calculate_g(y_new, y_d_new,M,m,L)
+    t_new = t + dt
+
+    return y_new, y_d_new, y_dd_new, t_new
 
 
-     
+
+#odeint solution
+sol = odeint (calculate_dydt,Y0,t, args=(M,m,L))
+
+#Runge-Kutta solution
+y_dd_arr[0] = calculate_g(y_arr[0], y_d_arr[0], M, m, L)
+for i in range(N-1):
+    y_arr[i+1], y_d_arr[i+1], y_dd_arr[i+1], time[i+1] = rungeKutta(dt, time[i], y_arr[i], y_d_arr[i], y_dd_arr[i], M, m, L)
 
 
-    return
+# Plot and compare
+plt.figure()
+plt.plot(t,sol[:,0],label = 'x odeint')
+plt.plot(t,sol[:,2],label = 'theta odeint')
+plt.plot(time,y_arr[:,0],'--', label='x')
+plt.plot(time,y_arr[:,1],'--', label='theta')
+plt.xlabel('Time')
+plt.ylabel('x, ${\\theta}$')
+plt.legend()
+plt.show()
+
+plt.figure()
+plt.plot(t,sol[:,1],label = 'x_d odeint')
+plt.plot(t,sol[:,3],label = 'theta_d odeint')
+plt.plot(time,y_d_arr[:,0], '--', label='x_d')
+plt.plot(time,y_d_arr[:,1], '--', label='theta_d')
+plt.xlabel('Time')
+plt.ylabel('$\dot{x}, \dot{\\theta}$')
+plt.legend()
+plt.show()
+
